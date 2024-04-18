5785a4df9951cd2e56c88ffc75e0e6b7599bfd64
Prince Raj <praj81232@gmail.com>
2024-04-18 18:30:16 +0530

    integrating

--- client/src/Api/ApiEndPoint.jsx
+++ client/src/Api/ApiEndPoint.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // http://localhost:5000/api
 const instances = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://mern-jwt-auth-frontend.vercel.app/api"
 })
 
 
